e024170d805b3570f581dc06691dd4b2405f21bd
Zachary Smith <billyzacsmith@gmail.com>
2015-10-23 16:59:36 -0600

    Add title to displayed feed.

--- js/nytimes-app3.js
+++ js/nytimes-app3.js
@@ -1,5 +1,3 @@
-$( document ).ready(function() {
-
 var globalData;
 
 // Style
@@ -19,14 +17,23 @@ var tileNum = 0; // For appending to the tile ID
 var nytimesBaseURI = "http://api.nytimes.com/svc/search/v2/articlesearch.response-format?";
 var appKey = "9d1a3ce50892530fe2b12b67ba93be86:17:72349747";
 
+var dataURI = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&page=' + '0' + '&api-key=9d1a3ce50892530fe2b12b67ba93be86:17:72349747';
+// Just for testing
+// $.getJSON(dataURI, function (data){
+//   var headline = data.response.docs[0].headline.name;
+//   console.log(headline);
+// });
 
 for (var pageNum = 0; pageNum<5; pageNum++) {
 var dataURI = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&page=' + pageNum + '&api-key=9d1a3ce50892530fe2b12b67ba93be86:17:72349747';
 
+// The real API call
 $.getJSON(dataURI, function (data){
 	globalData = data.response.docs;
 	$.each(data.response.docs, function(i, item){
-		var article = item.lead_paragraph.substring(0, 30);
+    var headline = item.headline.name;
+    console.log(headline);
+		var article = item.lead_paragraph;
 
 		// Create link tile to original article
 		var linkHTML = '<a href="';
@@ -35,7 +42,8 @@ $.getJSON(dataURI, function (data){
 
 		var innerHTML = '<div class="tile" id="tile-'; // Open the tile div
 			innerHTML += i; // Give tile a numbered id
-			innerHTML += '">'
+			innerHTML += '">';
+      innerHTML += '<h1>' + headline + '</h1>';
 			innerHTML += article;
 			innerHTML += '</div>'; // Close the tile div
 		$tileContainer.append( linkHTML + innerHTML + '</a>'); // Put the tile div on the page
@@ -44,5 +52,3 @@ $.getJSON(dataURI, function (data){
 	}); // end each
 }); // end getJSON
 }; //end pageNum for loop
-
-}) // end document ready
